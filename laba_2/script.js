156623ee76432eb1c1306fd52b607005180a462a
Nazar <nazariykharchuk@gmail.com>
2021-10-14 23:22:40 +0300

    Added the first check of the entered

--- laba_2/script.js
+++ laba_2/script.js
@@ -1,5 +1,45 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
-  alert("JS file added");
+  const form = document.getElementById("form");
+  form.addEventListener("submit", Send);
+
+  async function Send(e) {
+    e.preventDefault();
+
+    //let error = 
+    Validator();
+
+
+  }
 });
+
+function Validator() {
+  //let error = 0;
+  let req = document.querySelectorAll("._req");
+  for (let index = 0; index < req.length; index++) {
+    const input = req[index];
+    RemoveError(input);
+    if (input.classList.contains("_gmail")) {
+      if (ValidGmail(input)) {
+        AddError(input);
+        //error++;
+      }
+    } else if (input.value == "") {
+      AddError(input);
+      //error++;
+    }
+  }
+}
+
+function AddError(input) {
+  input.classList.add("_error");
+}
+
+function RemoveError(input) {
+  input.classList.remove("_error");
+}
+
+function ValidGmail(input) {
+  return !/^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+}
